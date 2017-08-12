934a13b7e26ea7099221e4845fa856a3cf72fec0
chrisyin <chrisyin.2vv@gmail.com>
2017-08-12 20:34:13 +0800

    FPV Success

--- app/src/main/java/hitsz/chris/spark_orbslam/MainActivity.java
+++ app/src/main/java/hitsz/chris/spark_orbslam/MainActivity.java
@@ -1,13 +1,178 @@
 package hitsz.chris.spark_orbslam;
 
+import android.app.Activity;
+import android.graphics.SurfaceTexture;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
-public class MainActivity extends AppCompatActivity {
+import dji.common.product.Model;
+import dji.sdk.base.BaseProduct;
+import dji.sdk.camera.Camera;
+import dji.sdk.camera.VideoFeeder;
+import dji.sdk.codec.DJICodecManager;
+
+public class MainActivity extends Activity implements TextureView.SurfaceTextureListener, View.OnClickListener  {
+    //UI variables
+    protected TextureView mVideoSurface = null;
+    private Button mCaptureBtn, mShootPhotoModeBtn, mRecordVideoModeBtn;
+    private ToggleButton mRecordBtn;
+    private TextView recordingTime;
+//Video
+    protected DJICodecManager mCodecManager = null;
+    private static final String TAG = MainActivity.class.getName();
+    protected VideoFeeder.VideoDataCallback mReceivedVideoDataCallBack = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
+        initUI();
+        // The callback for receiving the raw H264 video data for camera live view
+        mReceivedVideoDataCallBack = new VideoFeeder.VideoDataCallback() {
+            @Override
+            public void onReceive(byte[] videoBuffer, int size) {
+                if (mCodecManager != null) {
+                    mCodecManager.sendDataToDecoder(videoBuffer, size);
+                }
+            }
+        };
     }
+
+    protected void onProductChange() {
+        initPreviewer();
+    }
+    @Override
+    public void onResume() {
+        Log.e(TAG, "onResume");
+        super.onResume();
+        initPreviewer();
+        onProductChange();
+        if(mVideoSurface == null) {
+            Log.e(TAG, "mVideoSurface is null");
+        }
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+    public void onReturn(View view){
+        this.finish();
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+    @Override
+    public void onSurfaceTextureAvailable(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureAvailable");
+        if (mCodecManager == null) {
+            mCodecManager = new DJICodecManager(this, surface, width, height);
+        }
+    }
+    @Override
+    public void onSurfaceTextureSizeChanged(SurfaceTexture surface, int width, int height) {
+        Log.e(TAG, "onSurfaceTextureSizeChanged");
+    }
+    @Override
+    public boolean onSurfaceTextureDestroyed(SurfaceTexture surface) {
+        Log.e(TAG,"onSurfaceTextureDestroyed");
+        if (mCodecManager != null) {
+            mCodecManager.cleanSurface();
+            mCodecManager = null;
+        }
+        return false;
+    }
+    @Override
+    public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_capture:{
+                break;
+            }
+            case R.id.btn_shoot_photo_mode:{
+                break;
+            }
+            case R.id.btn_record_video_mode:{
+                break;
+            }
+            default:
+                break;
+        }
+    }
+
+//Initial UI Function
+    private void initUI() {
+        // init mVideoSurface
+        mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
+        recordingTime = (TextView) findViewById(R.id.timer);
+        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
+        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
+        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
+        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
+
+        if (null != mVideoSurface) {
+            mVideoSurface.setSurfaceTextureListener(this);
+        }
+
+        mCaptureBtn.setOnClickListener(this);
+        mRecordBtn.setOnClickListener(this);
+        mShootPhotoModeBtn.setOnClickListener(this);
+        mRecordVideoModeBtn.setOnClickListener(this);
+        recordingTime.setVisibility(View.INVISIBLE);
+        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+            }
+        });
+    }
+    //Show Video
+    private void initPreviewer() {
+        BaseProduct product = SparkApplication.getProductInstance();
+        if (product == null || !product.isConnected()) {
+            showToast(getString(R.string.disconnected));
+        } else {
+            if (null != mVideoSurface) {
+                mVideoSurface.setSurfaceTextureListener(this);
+            }
+            if (!product.getModel().equals(Model.UNKNOWN_AIRCRAFT)) {
+                if (VideoFeeder.getInstance().getVideoFeeds() != null
+                        && VideoFeeder.getInstance().getVideoFeeds().size() > 0) {
+                    VideoFeeder.getInstance().getVideoFeeds().get(0).setCallback(mReceivedVideoDataCallBack);
+                }
+            }
+        }
+    }
+    private void uninitPreviewer() {
+        Camera camera = SparkApplication.getCameraInstance();
+        if (camera != null){
+            // Reset the callback
+            VideoFeeder.getInstance().getVideoFeeds().get(0).setCallback(null);
+        }
+    }
+    //Show Message
+    public void showToast(final String msg) {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, msg, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
 }
